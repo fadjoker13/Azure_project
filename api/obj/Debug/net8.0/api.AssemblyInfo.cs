be2448bc90142458cb98ce75fa37bf5f915cbac3
hamza.belyahiaoui <hamza.belyahiaoui@spie.com>
2024-11-17 21:57:11 +0100

    fix  UseHttpsRedirection

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19cc5cc6575181e7e6f512c0a1e9982d1ddc5da8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b365d2585d5e4e6250b29235d417d9b661e4245")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
